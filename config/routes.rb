5f400ce7ed7580226fd1d5ceaea9a4e1a2f7eeab
anne laws <lawsannechristine@gmail.com>
2019-08-06 15:33:32 +0800

    Added pundit to grandma

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'grandmas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :grandmas
 end
