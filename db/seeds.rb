4c67d5337e7bef06721517430404a6dc30088a3c
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-06 12:10:49 +0800

    Skill creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+skills = Skill.create(name: "cooking")
+skills = Skill.create(name: "reading")
+skills = Skill.create(name: "knitting")
+skills = Skill.create(name: "story-telling")
+skills = Skill.create(name: "child-care")
+skills = Skill.create(name: "looking for glasses")
+skills = Skill.create(name: "motivating")
+skills = Skill.create(name: "wise commenter")
+skills = Skill.create(name: "telling people off")
+skills = Skill.create(name: "gardening")
+skills = Skill.create(name: "coffee time")
+skills = Skill.create(name: "playing cards")
