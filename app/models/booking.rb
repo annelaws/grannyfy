dc2cd9d6d6a4ed2b45d2809a23268dfa00e54930
anne laws <lawsannechristine@gmail.com>
2019-08-06 16:27:00 +0800

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  # belongs_to :grandma
-  # belongs_to :user
+  belongs_to :grandma
+  belongs_to :user
   validates :start_date, :end_date, presence: true
   validate :start_date_valid?, :end_date_valid?
   # custom validation method to check if start date and end date are correct
