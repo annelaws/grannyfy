3b20d4fd9c464420d4cd0db0ad38effe06f13a05
laura white <lauraeileen.white@gmail.com>
2019-08-06 10:13:56 +0200

    validations added to the modls

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :content, :score, presence: true
+  validates :score, numericality: true, inclusion: { in: %w(0 1 2 3 4 5),
+    message: "%{value} is not a valid score please rate from 0 to 5" }
 end
