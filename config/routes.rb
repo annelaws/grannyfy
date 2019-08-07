df29ddbfc5776f569f268fd52fed79e940794896
anne laws <lawsannechristine@gmail.com>
2019-08-07 11:03:46 +0800

    Profile page profile card done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :grandmas
+
+  get '/profile', to: 'users#show'
 end
