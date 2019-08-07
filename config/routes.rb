018954faef8e27fe19684d6dbaa2e1016d5acfa1
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-07 12:01:52 +0800

    added routes for booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :grandmas
+  resources :grandmas do
+    resources :bookings, only: [:show, :new, :create]
+  end
 end
+
