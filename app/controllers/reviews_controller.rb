ae8ee8301367b5d15805649cb6aa2aeaa1546f13
laura white <lauraeileen.white@gmail.com>
2019-08-07 20:07:17 +0200

    checkboxes on new grandma form + gradient button component added and some front modifications

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,17 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
   def new
+    @booking = Booking.find(params[:id])
+    @review = Review.new
   end
 
   def create
+    @booking = Booking.find(params[:id])
+    @review = Review.new(review_params)
+    @review.booking = @review
+    @review.save
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :score)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,14 +6,9 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-
-
   get '/profile', to: 'users#show'
-
-
-  get '/profile/bookings/:id', to: 'bookings#show', as: :booking_show do
-    resources :reviews, only: [:new, :create]
-  end
-
+  get '/profile/bookings/:id', to: 'bookings#show', as: :booking_show
+  get '/profile/bookings/:id/reviews/new', to:'reviews#new', as: :new_review
+  post '/profile/bookings/:id/reviews', to: 'reviews#create', as: :reviews
 end
 
