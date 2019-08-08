ebb9d3c0cccc9abd05ee392dd636dfbae49d7de8
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-08 15:48:00 +0800

    Search and skills grandma

--- app/models/grandma.rb
+++ app/models/grandma.rb
@@ -10,4 +10,22 @@ class Grandma < ApplicationRecord
   validates_uniqueness_of :location, scope: [:first_name, :last_name]
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  include PgSearch
+    pg_search_scope :search_by_first_name_and_last_name,
+    against: [ :first_name, :last_name ],
+    associated_against: {
+      skills: [ :name ],
+
+    },
+
+    using: {
+      tsearch: { prefix: true }
+    }
+
+
+    def set_grandma_skills(ids, grandma)
+      ids.each do |skill|
+        GrandmaSkill.create(skill_id: skill, grandma: grandma)
+      end
+    end
 end
