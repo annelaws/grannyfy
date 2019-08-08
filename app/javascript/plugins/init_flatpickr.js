af35feb497af55f4a2a178e68ca54135cde1efab
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-08 17:45:21 +0800

    error handling for flatpickr

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -3,8 +3,10 @@ import flatpickr from "flatpickr";
 const calendar = () => {
   const startDate = document.getElementById('booking_start_date');
   const endDate = document.getElementById('booking_end_date');
-  flatpickr(startDate, {});
-  flatpickr(endDate, {});
+  if (startDate) {
+    flatpickr(startDate, {});
+    flatpickr(endDate, {});
+  }
 };
 
 export { calendar };
